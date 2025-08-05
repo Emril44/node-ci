1b8627dfdeb666637931a6ae9549277b38f22497
M. K. <42112702+Emril44@users.noreply.github.com>
2025-08-05 20:36:44 +0300

    Creating Dockerfile

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello world!!!!!");
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.end('Hello from Node inside Docker :D');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
