658d6dd1d56f71e7b52664b4dcd35bf31145d733
M. K. <42112702+Emril44@users.noreply.github.com>
2025-08-06 03:19:43 +0300

    wait what (trying to push new ci/cd again)

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const http = require('http');
+const { Client } = require('pg');
 
 const PORT = process.env.PORT || 3000;
+
+const client = new Client({
+    host: 'db',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'mydb',
+});
+
+client.connect()
+    .then(() => console.log('Connected to Postgres!'))
+    .catch(err => console.error('Connection error! ', err));
 
 const server = http.createServer((req, res) => {
     res.end('this is node on docker, hello :D');
@@ -9,3 +21,4 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
