0c095e69e43329b101d80bfe93866765cd9abd34
M. K. <42112702+Emril44@users.noreply.github.com>
2025-08-07 19:43:02 +0300

    aaaaaaaaaugh

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const { Client } = require('pg');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const client = new Client({
     host: 'db',
